c263309f0eca47900f861a2c07c63f7e2ef00073
Rishabh Sahejpal <rishabhsahejpal@gmail.com>
2024-01-24 04:37:57 -0500

    Version 2.1.7.2

--- src/components/Space.js
+++ src/components/Space.js
@@ -27,12 +27,16 @@ export class Space extends Component {
     }
 
     getInfo = ()=>{
+        // // Pending usage of environment variables
+        // const REACT_APP_ISS_URL = "https://api.wheretheiss.at/v1/satellites/25544"
+        // const REACT_APP_LOCURL = "https://api.opencagedata.com/geocode/v1/json?"
+        // const REACT_APP_APIKEY = "9420a4fdb2ad41af8ac2890fa31f8c8c" 
+        
          //1st call
-        axios.get(REACT_APP_ISS_URL)
+        axios.get(process.env.REACT_APP_ISS_URL)
         .then(res => {
-            console.log(res,"1")
            // run a call to get the location info
-            let locURI = `${REACT_APP_LOCURL}q=${res.data.latitude}+${res.data.longitude}&key=${REACT_APP_APIKEY}`
+            let locURI = `${process.env.REACT_APP_LOCURL}q=${res.data.latitude}+${res.data.longitude}&key=${process.env.REACT_APP_APIKEY}`
             this.setState({
                 lat : res.data.latitude.toFixed(3),
                 lon : res.data.longitude.toFixed(3),
